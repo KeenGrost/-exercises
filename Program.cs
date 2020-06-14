c20e8ec20e171bdfb10a494cb33269219c42dd4c
moxiemaks.ru <keengrost@gmail.com>
2020-06-14 22:19:25 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawSnake(5);
+            DrawZiggurat(6);
+        }
+
+        private static void DrawSnake(int width)
+        {
+            Console.WriteLine($"Hello Snake! {width}");
+        }
+        private static void DrawZiggurat(int width)
+        {
+            Console.WriteLine($"Hello Ziggurat! {width}");
+        }
+    }
+}
